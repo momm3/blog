51d1db31b88205a95959ef8fce4fa3142ddab9df
Marvin Scott <momm3iskool@gmail.com>
2016-05-25 18:37:09 -0400

    inital commit

---
+++ db/migrate/20160525214651_create_coments.rb
@@ -0,0 +1,11 @@
+class CreateComents < ActiveRecord::Migration
+  def change
+    create_table :coments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :article, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
